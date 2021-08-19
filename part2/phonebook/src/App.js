c172a92c91ad9f5c852228abaac0ba0013dd24bf
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-19 14:56:36 +0300

    Solution for 2_9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchString, setSearchString ] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -30,9 +31,20 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const handleSearchStringChange = (event) => {
+    setSearchString(event.target.value)
+  }
+
+  const filteredPersons = persons.filter(person => {
+    return person.name.toLowerCase().includes(searchString.toLowerCase())
+  })
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          filter shown with<input value={searchString} onChange={handleSearchStringChange}/>
+      </div>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -45,7 +57,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+      {filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
     </div>
   )
 }
