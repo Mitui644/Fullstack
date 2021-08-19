2b15cc557580063ff6a012d1060fd4ba0eb13e5d
Michel Tuijula <michel.tuijula@hotmail.com>
2021-08-19 15:18:54 +0300

    Solution for 2_10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,35 @@
 import React, { useState } from 'react'
 
+const Filter = ({ text, callBack}) => (
+  <div>
+  filter shown with<input value={text} onChange={callBack}/>
+</div>
+)
+
+const PersonForm = ({ submitCallBack, nameCallback, numberCallback, name, number}) => (
+  <form onSubmit={submitCallBack}>
+  <div>
+    name: <input value={name} onChange={nameCallback}/>
+  </div>
+  <div>
+    number: <input value={number} onChange={numberCallback}/>
+  </div>
+  <div>
+    <button type="submit">add</button>
+  </div>
+  </form>
+)
+
+const Persons = ({ persons }) => (
+  persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)
+)
+
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
@@ -42,23 +69,16 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          filter shown with<input value={searchString} onChange={handleSearchStringChange}/>
-      </div>
-      <h2>add a new</h2>
-      <form onSubmit={addPerson}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {filteredPersons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}
+
+      <Filter text={searchString} callBack={handleSearchStringChange}/>
+
+      <h3>add a new</h3>
+
+      <PersonForm submitCallBack={addPerson} nameCallback={handleNameChange} numberCallback={handleNumberChange} name={newName} number={newNumber}/>
+
+      <h3>Numbers</h3>
+
+      <Persons persons={filteredPersons}/>
     </div>
   )
 }
